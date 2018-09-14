98b32c2f924567fbf5b1ce471df96950a77824c5
rem42 <me@remy.ovh>
2018-09-14 12:25:35 +0200

    Update soap method

---
+++ Request/IRequestSoap.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Scraper\Scraper\Request;
+
+interface IRequestSoap
+{
+	public function isDoRequest(): bool;
+	public function getAction(): string;
+	public function getVersion(): string;
+}
---
+++ Request/RequestSoap.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Scraper\Scraper\Request;
+
+abstract class RequestSoap extends Request implements IRequestSoap
+{
+
+}
