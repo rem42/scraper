5bbca29ceb4026dba36760cb45267b4ce2051e5f
rem42 <me@remy.ovh>
2019-08-05 14:40:36 +0200

    Update client transform contentType

--- Client.php
+++ Client.php
@@ -12,98 +12,105 @@ use Scraper\Scraper\Request\Request;
 
 class Client
 {
-	/**
-	 * @var bool
-	 */
-	protected $cache = false;
+    /**
+     * @var bool
+     */
+    protected $cache = false;
 
-	/**
-	 * @var Cache
-	 */
-	protected $cacheModel;
+    /**
+     * @var Cache
+     */
+    protected $cacheModel;
 
-	/**
-	 * @param Request $request
-	 *
-	 * @return mixed
-	 * @throws \Doctrine\Common\Annotations\AnnotationException
-	 * @throws \ReflectionException
-	 */
-	public function api(Request $request)
-	{
-		$reflectClassRequest = new \ReflectionClass($request);
-		$className           = $reflectClassRequest->getShortName();
-		$className           = str_replace('Request', 'Api', $className);
-		$namespace           = str_replace('\Request', '\Api\\', $reflectClassRequest->getNamespaceName());
-		/** @var UrlAnnotation $urlAnnotation */
-		$urlAnnotation = Reader::read($request);
-		$data          = $this->fetchData($request, $reflectClassRequest, $urlAnnotation);
+    /**
+     * @param Request $request
+     *
+     * @return mixed
+     *
+     * @throws \Doctrine\Common\Annotations\AnnotationException
+     * @throws \ReflectionException
+     */
+    public function api(Request $request)
+    {
+        $reflectClassRequest = new \ReflectionClass($request);
+        $className           = $reflectClassRequest->getShortName();
+        $className           = str_replace('Request', 'Api', $className);
+        $namespace           = str_replace('\Request', '\Api\\', $reflectClassRequest->getNamespaceName());
+        /** @var UrlAnnotation $urlAnnotation */
+        $urlAnnotation = Reader::read($request);
+        $data          = $this->fetchData($request, $reflectClassRequest, $urlAnnotation);
 
-		$reflectClassName = new \ReflectionClass($namespace . $className);
-		$instance         = $reflectClassName->newInstanceArgs([$request, $data, $urlAnnotation]);
-		return call_user_func([$instance, 'execute']);
-	}
+        $reflectClassName = new \ReflectionClass($namespace . $className);
+        $instance         = $reflectClassName->newInstanceArgs([$request, $data, $urlAnnotation]);
+        return call_user_func([$instance, 'execute']);
+    }
 
-	/**
-	 * @return $this
-	 */
-	public function enableCache(Cache $cache)
-	{
-		$this->cache      = true;
-		$this->cacheModel = $cache;
-		return $this;
-	}
+    /**
+     * @return $this
+     */
+    public function enableCache(Cache $cache)
+    {
+        $this->cache      = true;
+        $this->cacheModel = $cache;
+        return $this;
+    }
 
-	/**
-	 * @param Request          $request
-	 * @param \ReflectionClass $reflectionClass
-	 * @param UrlAnnotation    $urlAnnotation
-	 *
-	 * @return Response
-	 */
-	private function fetchData(Request $request, \ReflectionClass $reflectionClass, UrlAnnotation $urlAnnotation)
-	{
-		if($this->cache && $this->cacheModel instanceof Cache){
-			$this->cacheModel->setRequest($request);
-			$this->cacheModel->setReflectionClass($reflectionClass);
-		}else{
-			$this->cache = false;
-		}
-		try {
-			$cacheRequest = false;
-			if ($this->cache && $this->cacheModel->exist()) {
-				$cacheRequest = true;
-				$response     = new Response(200, [], $this->cacheModel->get());
-			}
-			if (!$cacheRequest) {
-				/** @var IProtocol $class */
-				$class = (new \ReflectionClass('\Scraper\\Scraper\\Protocol\\' . ucfirst(strtolower($urlAnnotation->protocol))))
-					->newInstanceArgs([$reflectionClass, $request, $urlAnnotation]);
-				/** @var Response $response */
-				$response = $class->execute();
-			}
-		} catch (\Exception $e) {
-			echo 'Exception: ', $e->getMessage(), "\n";
-		}
+    /**
+     * @param Request          $request
+     * @param \ReflectionClass $reflectionClass
+     * @param UrlAnnotation    $urlAnnotation
+     *
+     * @return Response
+     */
+    private function fetchData(Request $request, \ReflectionClass $reflectionClass, UrlAnnotation $urlAnnotation)
+    {
+        if ($this->cache && $this->cacheModel instanceof Cache) {
+            $this->cacheModel->setRequest($request);
+            $this->cacheModel->setReflectionClass($reflectionClass);
+        } else {
+            $this->cache = false;
+        }
 
-		try {
-			$contentType = $request->getContentType() ?? $urlAnnotation->contentType;
-			if (in_array(strtolower($contentType), ['html', 'json', 'text', 'xml'])) {
-				/** @var IContentType $class */
-				$class = (new \ReflectionClass('\Scraper\\Scraper\\ContentType\\' . ucfirst(strtolower($contentType))))
-					->newInstanceArgs([$reflectionClass, $request, $response, $urlAnnotation, $this->cacheModel]);
-			} else {
-				/** @var IContentType $class */
-				$class = (new \ReflectionClass($urlAnnotation->contentType))
-					->newInstanceArgs([$reflectionClass, $request, $response, $urlAnnotation]);
-			}
+        try {
+            $cacheRequest = false;
 
-			/** @var Response $response */
-			$response = $class->execute();
-		} catch (\Exception $e) {
-			echo 'Exception: ', $e->getMessage(), "\n";
-		}
+            if ($this->cache && $this->cacheModel->exist()) {
+                $cacheRequest = true;
+                $response     = new Response(200, [], $this->cacheModel->get());
+            }
 
-		return $response;
-	}
+            if (!$cacheRequest) {
+                /** @var IProtocol $class */
+                $class = (new \ReflectionClass('\Scraper\\Scraper\\Protocol\\' . ucfirst(strtolower($urlAnnotation->protocol))))
+                    ->newInstanceArgs([$reflectionClass, $request, $urlAnnotation]);
+                /** @var Response $response */
+                $response = $class->execute();
+            }
+        } catch (\Exception $e) {
+            echo 'Exception (request): ', $e->getMessage(), "\n";
+        }
+
+        try {
+            if (in_array(strtolower($request->getContentType()), ['html', 'json', 'text', 'xml'])) {
+                /** @var IContentType $class */
+                $class = (new \ReflectionClass('\Scraper\\Scraper\\ContentType\\' . ucfirst(strtolower($request->getContentType()))))
+                    ->newInstanceArgs([$reflectionClass, $request, $response, $urlAnnotation, $this->cacheModel]);
+            } elseif (in_array(strtolower($urlAnnotation->contentType), ['html', 'json', 'text', 'xml'])) {
+                /** @var IContentType $class */
+                $class = (new \ReflectionClass('\Scraper\\Scraper\\ContentType\\' . ucfirst(strtolower($urlAnnotation->contentType))))
+                    ->newInstanceArgs([$reflectionClass, $request, $response, $urlAnnotation, $this->cacheModel]);
+            } else {
+                /** @var IContentType $class */
+                $class = (new \ReflectionClass($urlAnnotation->contentType))
+                    ->newInstanceArgs([$reflectionClass, $request, $response, $urlAnnotation]);
+            }
+
+            /** @var Response $response */
+            $response = $class->execute();
+        } catch (\Exception $e) {
+            echo 'Exception (data): ', $e->getMessage(), "\n";
+        }
+
+        return $response;
+    }
 }
