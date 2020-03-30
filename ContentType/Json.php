29f3b1b61b9dde15a0cd2b1db8148b028fccd144
rem42 <rbruyere.externe@m6web.fr>
2020-03-30 18:56:56 +0200

    remove dump

--- ContentType/Json.php
+++ ContentType/Json.php
@@ -7,11 +7,8 @@ class Json extends ContentType
     public function execute()
     {
         $body    = $this->response->getBody()->getContents();
-        dump($body);
         $content = json_decode($body, true);
         $this->setCache($body);
-        dump($content);
-        die('');
 
         if (JSON_ERROR_NONE === json_last_error()) {
             return $content;
