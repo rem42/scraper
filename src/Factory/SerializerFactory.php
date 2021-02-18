7cb1df5f809484cf8a1337d1e374d48cf01be28d
rem42 <me@remy.ovh>
2021-02-18 21:09:06 +0100

    feat(serializer): use factory to create it

---
+++ src/Factory/SerializerFactory.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Scraper\Scraper\Factory;
+
+use Doctrine\Common\Annotations\AnnotationReader;
+use Symfony\Component\PropertyAccess\PropertyAccess;
+use Symfony\Component\PropertyInfo\Extractor\PhpDocExtractor;
+use Symfony\Component\PropertyInfo\Extractor\ReflectionExtractor;
+use Symfony\Component\PropertyInfo\PropertyInfoExtractor;
+use Symfony\Component\Serializer\Encoder\JsonEncoder;
+use Symfony\Component\Serializer\Encoder\XmlEncoder;
+use Symfony\Component\Serializer\Mapping\Factory\ClassMetadataFactory;
+use Symfony\Component\Serializer\Mapping\Loader\AnnotationLoader;
+use Symfony\Component\Serializer\NameConverter\CamelCaseToSnakeCaseNameConverter;
+use Symfony\Component\Serializer\NameConverter\MetadataAwareNameConverter;
+use Symfony\Component\Serializer\Normalizer\ArrayDenormalizer;
+use Symfony\Component\Serializer\Normalizer\DataUriNormalizer;
+use Symfony\Component\Serializer\Normalizer\DateTimeNormalizer;
+use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
+use Symfony\Component\Serializer\Normalizer\PropertyNormalizer;
+use Symfony\Component\Serializer\Serializer;
+
+class SerializerFactory
+{
+    public static function create(): Serializer
+    {
+        $classMetadataFactory = new ClassMetadataFactory(new AnnotationLoader(new AnnotationReader()));
+
+        $metadataAwareNameConverter = new MetadataAwareNameConverter($classMetadataFactory, new CamelCaseToSnakeCaseNameConverter());
+
+        $extractor = new PropertyInfoExtractor([], [new PhpDocExtractor(), new ReflectionExtractor()]);
+
+        $encoders    = ['json' => new JsonEncoder(), 'xml' => new XmlEncoder()];
+        $normalizers = [
+            new ArrayDenormalizer(),
+            new ObjectNormalizer($classMetadataFactory, $metadataAwareNameConverter, PropertyAccess::createPropertyAccessor(), $extractor),
+            new PropertyNormalizer(),
+            new DateTimeNormalizer(),
+            new DataUriNormalizer(),
+        ];
+
+        return new Serializer($normalizers, $encoders);
+    }
+}
