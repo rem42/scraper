d06dea8982cbd7ec77a19d46197a9155484c85be
rem42 <me@remy.ovh>
2021-04-12 22:25:44 +0200

    fix(query): array annotation phpstan

--- src/Request/RequestQuery.php
+++ src/Request/RequestQuery.php
@@ -5,7 +5,7 @@ namespace Scraper\Scraper\Request;
 interface RequestQuery
 {
     /**
-     * @return array<string, string>
+     * @return array<int|string, string>
      */
     public function getQuery(): array;
 }
